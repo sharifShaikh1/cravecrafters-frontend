51f47d259c87aade057a8bbdb1ef92d354a51264
sharifShaikh1 <sharifshaikh3534@gmail.com>
2025-05-15 22:58:14 +0530

    Update api.js

--- src/components/api.js
+++ src/components/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://cravecrafters-backend.onrender.com',
   timeout: 15000, // Increased timeout
   withCredentials: true,
 });
